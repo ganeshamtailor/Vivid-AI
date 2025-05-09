c5b3cdedabc793f1c732fea6496f7a63b75394e9
Ganesham <ganeshamtailor@gmail.com>
2025-05-09 17:27:45 +0530

    added sign-in and sign-up page

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const Signup = () => {
+      return (
+    <div className="p-4 bg-transparent">
+      <SignUp />
+    </div>
+  )
+}
+
+export default Signup
